88d6cb44c05de4545d8239d045797048fab9ccb0
Antonio Runcio <runcioantonio@gmail.com>
2022-02-04 19:27:00 +0100

    inizio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// src\App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const title = "BLOG POST";
+
+  const body = "This is my blog post";
+
+  const comments = [
+    { id: 1, text: "Comment one" },
+    { id: 2, text: "Comment two" },
+    { id: 3, text: "Comment three" },
+  ];
+
+  //   Esecuzione condizionale
+  const loading = false;
+  if (loading) return <h1>Loading....</h1>;
+
+  const showcomments = true;
+
+  const commentBlock = (
+    <ul>
+      {/* Faccio il map per ottenere la lista degli elementi dall'array */}
+      {comments.map((comment, index) => (
+        <li key={index}>{comment.text}</li>
+      ))}
+    </ul>
   );
-}
+
+  return (
+    //   Uso dei fragment
+    <>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      <h3>Comments ({comments.length})</h3>
+
+      {/* Utilizzo l'istruzione AND per fare una visualizzazione condizionale */}
+      {showcomments && commentBlock}
+    </>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// src\index.js
 
-ReactDOM.render(
+import React from "react";
+
+import ReactDom from "react-dom";
+
+import App from "./App";
+
+import "./index.css";
+
+// ReactDom render permette di interagire con il DOM
+// Scelgo prima l'elemento da visualizzare e poi dove visualizzarlo.
+
+ReactDom.render(
+//   Attivazione dello Strict mode
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
