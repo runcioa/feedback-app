b3bac1b542bdf42e66751ac3fbc6b2490cffc897
Antonio Runcio <runcioantonio@gmail.com>
2022-02-04 19:28:55 +0100

    inizio

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
 // src\App.js
 
 const App = () => {
-  const title = "BLOG POST";
-
-  const body = "This is my blog post";
-
-  const comments = [
-    { id: 1, text: "Comment one" },
-    { id: 2, text: "Comment two" },
-    { id: 3, text: "Comment three" },
-  ];
-
-  //   Esecuzione condizionale
-  const loading = false;
-  if (loading) return <h1>Loading....</h1>;
-
-  const showcomments = true;
-
-  const commentBlock = (
-    <ul>
-      {/* Faccio il map per ottenere la lista degli elementi dall'array */}
-      {comments.map((comment, index) => (
-        <li key={index}>{comment.text}</li>
-      ))}
-    </ul>
-  );
-
   return (
-    //   Uso dei fragment
-    <>
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      <h3>Comments ({comments.length})</h3>
-
-      {/* Utilizzo l'istruzione AND per fare una visualizzazione condizionale */}
-      {showcomments && commentBlock}
-    </>
+    <div className="container">
+      <h1>My App</h1>
+    </div>
   );
 };
 
