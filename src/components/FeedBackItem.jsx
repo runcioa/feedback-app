05e9aaff86f080c5330269141b8615cb52f62b85
Antonio Runcio <runcioantonio@gmail.com>
2022-02-06 15:25:02 +0100

    Conditional style

--- src/components/FeedBackItem.jsx
+++ src/components/FeedBackItem.jsx
@@ -5,7 +5,7 @@ import Card from "./shared/Card";
 function FeedBackItem({ item }) {
   console.log(item);
   return (
-    <Card reverse = {true}>
+    <Card>
       <div className="num-display">{item.rating}</div>
       <div className="text-diplay">{item.text}</div>
     </Card>
--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -6,4 +6,9 @@ function Card({ children, reverse }) {
   return <div className={`card ${reverse && 'reverse'}`}>{children}</div>;
 }
 
+
+Card.defaultProps = {
+    reverse: false,
+};
+
 export default Card;
