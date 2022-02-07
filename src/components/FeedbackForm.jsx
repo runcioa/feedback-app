29f4cd83954d59abf40b78c67b0718157c319aba
Antonio Runcio <runcioantonio@gmail.com>
2022-02-07 10:12:49 +0100

    real time form validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,30 +1,47 @@
 // src\components\FeedbackForm.jsx
 
-import {useState} from 'react';
-import Card from './shared/Card';
-import Button from './shared/Button';
+import { useState } from "react";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
 
 function FeedbackForm() {
-    const [text, setText] = useState('');
+  const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
-    const handleTextChange = (event)=>{
-        setText(event.target.value);
-    }
+  const handleTextChange = (event) => {
+      if (text === '') {
+          setBtnDisabled(true);
+          setMessage(null);
+      } else if(text !== '' && text.trim().length <=10){
+          setMessage('Text must be al least 10 character');
+          setBtnDisabled(true);
+      } else {
+          setMessage(null);
+          setBtnDisabled(false);
+      }
+    setText(event.target.value);
+  };
 
-  return <Card>
+  return (
+    <Card>
       <form>
-          <h2>ahdkajdkjsa</h2>
-          <div className="input-group">
-              <input onChange={handleTextChange} type="text" 
-              placeholder='Write a review'
-              value={text} />
-              <Button type='submit'
-              version='secondary'
-              
-                >Send</Button>
-          </div>
+        <h2>ahdkajdkjsa</h2>
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type="submit" version="secondary" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
       </form>
-  </Card>;
+    </Card>
+  );
 }
 
 export default FeedbackForm;
