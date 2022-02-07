3c367317f012884a1ffe09db76e67b1002649d77
Antonio Runcio <runcioantonio@gmail.com>
2022-02-07 13:19:14 +0100

    Framer motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,5 +1,6 @@
 // src\components\FeedbackList.jsx
 
+import { motion, AnimatePresence } from 'framer-motion';
 import React from 'react';
 import FeedBackItem from './FeedBackItem';
 
@@ -10,10 +11,25 @@ function FeedbackList({feedBack, handleDelete}) {
   }
 
   return <div className='feedback-list'>
+      <AnimatePresence>
       {feedBack.map((item)=>(
+        <motion.div 
+        key={item.id}
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        exit={{opacity: 0}}>
+          
         <FeedBackItem key={item.id} item={item} handleDelete={handleDelete}/>
+        </motion.div>
       ))}
+      </AnimatePresence>
   </div>;
+
+// return <div className='feedback-list'>
+// {feedBack.map((item)=>(
+//   <FeedBackItem key={item.id} item={item} handleDelete={handleDelete}/>
+// ))}
+// </div>;
 }
 
 export default FeedbackList;
