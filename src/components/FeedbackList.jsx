72e94fea30e26a387a88a3275be2ec6814b6c351
Antonio Runcio <runcioantonio@gmail.com>
2022-02-10 11:26:57 +0100

    Spinner component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,27 +1,28 @@
 // src\components\FeedbackList.jsx
 
-//Importo il context 
-import {useContext} from 'react';
-import FeedbackContext from '../context/FeedbackContext';
-
-import FeedBackItem from './FeedBackItem';
-
+//Importo il context
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
+import FeedBackItem from "./FeedBackItem";
 
 function FeedbackList() {
-
   // Recupero il context
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0){
-    return <p>No Feedback Yet</p>
+  if (!isLoading && (!feedback || feedback.length === 0)) {
+    return <p>No Feedback Yet</p>;
   }
 
-
-return <div className='feedback-list'>
-{feedback.map((item)=>(
-  <FeedBackItem key={item.id} item={item} />
-))}
-</div>;
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedBackItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
 }
 
 export default FeedbackList;
